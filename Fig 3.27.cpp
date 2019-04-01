ccfa5aeb36a7c7d9addf43a0a86997edcd739255
Bruno Heinzelmann Vieira <brunoheineng@gmail.com>
2019-03-31 23:40:10 -0300

    commit all

---
+++ Fig 3.27.cpp
@@ -0,0 +1,45 @@
+//Usando gabarito de função;
+
+#include <iostream>
+
+using namespace std;
+
+template <class T>
+
+T maximum( T value1, T value2, T value3)
+{
+    T max = value1;
+
+    if(value2 > max)
+        max = value2;
+
+    if(value3 > max)
+        max = value3;
+    return max;
+}
+
+int main()
+{
+    int int1, int2, int3;
+
+    cout << "Forneca tres valores inteiros: ";
+    cin >> int1 >> int2 >> int3;
+    cout << "O maior valor inteiro e: "
+         << maximum( int1, int2, int3);
+
+    double double1, double2, double3;
+
+    cout <<"\nForneca tres valores double: ";
+    cin >> double1 >> double2 >> double3;
+    cout <<"O maior valor double e: "
+        << maximum(double1, double2, double3);
+
+   char char1, char2, char3;
+
+   cout << "\nForneca tres caracteres: ";
+   cin >> char1 >> char2 >> char3;
+   cout << "O maior caractere e: "
+        <<maximum(char1, char2, char3)<< endl;
+
+    return 0;
+}
